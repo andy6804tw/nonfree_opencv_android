f51fd9a0bfeb251ae45fbd4404b2be5b826d9e8f
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2021-03-16 14:22:29 +0800

    add OpenCV SDK 2411

--- obj/local/x86/objs/nonfree/surf.o.d
+++ obj/local/x86/objs/nonfree/surf.o.d
@@ -1,43 +1,43 @@
 ./obj/local/x86/objs/nonfree/surf.o: jni/surf.cpp jni/precomp.hpp \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/opencv_modules.hpp \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/nonfree/nonfree.hpp \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/nonfree/features2d.hpp \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/opencv_modules.hpp \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/nonfree/nonfree.hpp \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/nonfree/features2d.hpp \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
   /Users/yilintsai/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
   /Users/yilintsai/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
   /Users/yilintsai/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
   /Users/yilintsai/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
   /Users/yilintsai/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/map \
   /Users/yilintsai/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
   /Users/yilintsai/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/limits \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/config.h \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
-  /Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/internal.hpp
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/flann/config.h \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+  OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/internal.hpp
 
 jni/precomp.hpp:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/opencv_modules.hpp:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/opencv_modules.hpp:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/nonfree/nonfree.hpp:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/nonfree/nonfree.hpp:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/nonfree/features2d.hpp:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/nonfree/features2d.hpp:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
 
 /Users/yilintsai/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
 
@@ -51,22 +51,22 @@ jni/precomp.hpp:
 
 /Users/yilintsai/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
 
 /Users/yilintsai/Library/Android/sdk/ndk/16.1.4479499/sources/cxx-stl/gnu-libstdc++/4.9/include/limits:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/config.h:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/flann/config.h:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
 
-/Users/yilintsai/Desktop/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/internal.hpp:
+OpenCV-2.4.11-android-sdk/sdk/native/jni/include/opencv2/core/internal.hpp:
